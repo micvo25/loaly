21e9cdaa4b23a97f03e021b5aa6737d1680755ed
Salvatore Darmetta <sdarmetta@gmail.com>
2024-01-23 00:51:12 -0500

    Initial Commit

---
+++ loaly/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  loaly
+//
+//  Created by Salvatore D'Armetta on 1/23/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ loaly/loalyApp.swift
@@ -0,0 +1,17 @@
+//
+//  loalyApp.swift
+//  loaly
+//
+//  Created by Salvatore D'Armetta on 1/23/24.
+//
+
+import SwiftUI
+
+@main
+struct loalyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
